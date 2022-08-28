7ab77b1bbcf73a6e97333790e494b2ccd947e49a
udbhavgoyal3358 <107344327+udbhavgoyal3358@users.noreply.github.com>
2022-08-28 11:49:08 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,47 +1,114 @@
-var sea,ship;
-var seaImg,shipImg;
+var garden,rabbit,apple,orangeL,redL;
+var gardenImg,rabbitImg,carrotImg,orangeImg,redImg;
+
 
 function preload(){
-   
-
-  //shipImg1 = loadAnimation("ship-1.png");
-  //shipImg1 = loadAnimation("ship-1.png");
-  //shipImg1 = loadAnimation("ship-1");
-  shipImg = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
-  //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
-  
-  seaImg = loadImage("sea.png");
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+  orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png");
 }
 
+
 function setup(){
+  
   createCanvas(400,400);
-  background("blue");
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
 
-  
-  sea=createSprite(400,200);
-  sea.addImage(seaImg);
-  sea.velocityX = -5;
-  sea.scale=0.3;
 
-  
-  ship = createSprite(130,200,30,30);
-  ship.addAnimation("movingShip",shipImg);
-  ship.scale =0.25;
-  
+//creating boy running
+rabbit = createSprite(160,340,20,20);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
 }
 
 function draw() {
   background(0);
-  sea.velocityX = -3;
+  
+  // boy moving on Xaxis with mouse'
+  rabbit.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+  
+   drawSprites();
+   
+  
+// var select_sprites = Math(random(1,3));
+
+var select_sprites = Math.random(random(1,3));
+
+// var select_sprites = Math.round(1,3);
+
+// var select_sprites = Math.round(random(1,3));
 
   
-  if(sea.x < 0){
-    //sea.x = 0;
-    sea.x = sea.width;
-    //sea.x = sea.width/8;
-    //sea.y = height;
-  }
+  // if (frameCount % 50 == 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //     createRed();
+  //   }
+  // }
 
- 
-  drawSprites();
+ if (frameCount % 80 == 0) {
+     if (select_sprites == 1) {
+       createApples();
+     } else if (select_sprites == 2) {
+       createOrange();
+     }
+   }
+
+  // if (frameCount / 80 == 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //     createRed();
+  //   }
+  // }
+
+   //if (frameCount % 80 = 0) {
+     //if (select_sprites == 1) {
+      // createApples();
+     //} else if (select_sprites == 2) {
+     //  createOrange();
+     //}else {
+      // createRed();
+    // }
+   //}
+
+
+
+}
+
+function createApples() {
+apple = createSprite(random(50, 350),40, 10, 10);
+apple.addImage(appleImg);
+apple.scale=0.07;
+apple.velocityY = 3;
+apple.lifetime = 150;
+  
+}
+
+function createOrange() {
+orangeL = createSprite(random(50, 350),40, 10, 10);
+orangeL.addImage(orangeImg);
+orangeL.scale=0.08;
+orangeL.velocityY = 3;
+orangeL.lifetime = 150;
+}
+
+function createRed() {
+redL = createSprite(random(50, 350),40, 10, 10);
+redL.addImage(redImg);
+redL.scale=0.06;
+  redL.velocityY = 3;
+  redL.lifetime = 150;
 }
